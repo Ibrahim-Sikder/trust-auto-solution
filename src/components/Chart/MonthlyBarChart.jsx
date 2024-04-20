5fa4a0c7fc2484a2c63e6c0cf00d51ebea3011c5
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-04-20 14:46:13 +0600

    sales-overview-chart-add

--- src/components/Chart/MonthlyBarChart.jsx
+++ src/components/Chart/MonthlyBarChart.jsx
@@ -4,11 +4,11 @@ export default function StackBars() {
   return (
     <BarChart
       series={[
-        { data: [3, 4, 4, 6, 5], stack: 'A', label: 'January' },
-        { data: [4, 3, 1, 5, 8], stack: 'A', label: 'February ' },
-        { data: [4, 2, 5, 4, 1], stack: 'B', label: 'March' },
+        { data: [7, 8, 4, 6, 5], stack: 'A', label: 'January' },
+        { data: [8, 3, 1, 5, 8], stack: 'A', label: 'February ' },
+        { data: [4, 1, 3, 4, 1], stack: 'B', label: 'March' },
         { data: [2, 8, 1, 3, 1], stack: 'B', label: 'April' },
-        { data: [10, 6, 5, 8, 9], label: 'May ' },
+        { data: [5, 5, 5, 8, 9], label: 'May ' },
       ]}
      
       height={450}
